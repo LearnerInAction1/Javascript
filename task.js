9cbe351f46e3d0e012a9282887273705d4b3bbc8
unknown <muhammad31470@iqraisb.edu.pk>
2024-03-05 01:12:35 -0800

    Task JavaScript

---
+++ task.js
@@ -0,0 +1,27 @@
+var length = 10;
+var width = 40;
+
+var area =length *width;
+console.log("The area of the rectangle is: " + area);
+
+
+var radius = 8;
+var area = Math.PI * radius * radius;
+console.log("The area of the circle is: " + area);
+
+
+var base = 10;
+var height = 20;
+var area = 0.6* base * height;
+
+console.log("The area of the triangle is: " + area);
+
+var fahrenheit = 70;
+var celsius = (5/9) * (fahrenheit - 32);
+
+console.log(fahrenheit + "°F is equal to " + celsius.toFixed(2) + "°C");
+
+
+var celsius = 50;
+var fahrenheit = (9/5) * celsius + 32;
+console.log(celsius + "°C is equal to " + fahrenheit.toFixed(2) + "°F");
